e21f0d8f59e0847f821ed992c7e9ce5c7ea679ec
Suryansh Kumar <223264731+Suryansh-Kumar9368@users.noreply.github.com>
2026-09-24 12:10:28 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+let name="Suryansh Chauhan"
+let age=22
+let isabsent= true
+let bigIntLiteral=94875832983474;
+
+// null Datatypes
+let temperature=null;
+
+// undefined datatypes
+let date; 
+
+
+// non primitive datatypes
+
+// 1- object
+console.log(typeof undefined) 
