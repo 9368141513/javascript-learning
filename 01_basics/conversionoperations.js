fcbbc0a4c6570a9c368e94355120fc8db594595f
Suryansh Kumar <223264731+Suryansh-Kumar9368@users.noreply.github.com>
2026-09-26 10:05:05 +0530

    conversion of Operations

---
+++ 01_basics/conversionoperations.js
@@ -0,0 +1,32 @@
+let score="33abc"
+
+console.log(typeof score);
+
+// conversion into number
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+// NaN is not number is show when score variable have proper number 
+
+/*
+"33"= 33
+"33abc"=NaN
+true= 1; false=0
+
+*/
+
+let isLoggesIn=0
+let booleanIsLoggedIn= Boolean(isLoggesIn)
+console.log(booleanIsLoggedIn)
+
+// 1= tue;0=false
+// ""= false
+// "suryansh"= true
+
+
+
+let somenumber=33
+
+let  stringNumber=String(somenumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
